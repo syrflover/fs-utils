f7b13b3fa081006a61553e66374824c7e19b947d
Lee Tae Woo <syr@meu.works>
2019-10-14 21:30:10 +0900

    use namespace

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import * as fs from 'fs';
 
-import { readFile } from './lib/readFile';
-import { writeFile } from './lib/writeFile';
-import { exists } from './lib/exists';
-import { rm } from './lib/rm';
+import { readFile as _readFile } from './lib/readFile';
+import { writeFile as _writeFile } from './lib/writeFile';
+import { exists as _exists } from './lib/exists';
+import { rm as _rm } from './lib/rm';
 
-export default {
-    exists,
-    readFile,
-    writeFile,
-    rm,
-    mkdir: fs.promises.mkdir,
-    rename: fs.promises.rename,
-};
+// tslint:disable-next-line: no-namespace
+namespace _fs {
+    export const readFile = _readFile;
+    export const writeFile = _writeFile;
+    export const exists = _exists;
+    export const rm = _rm;
+    export const mkdir = fs.promises.mkdir;
+    export const rename = fs.promises.rename;
+}
+
+export default _fs;
